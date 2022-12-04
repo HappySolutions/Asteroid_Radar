7b38104a024597a5372f8423322bef1245475150
HappySolutions <heba.odesk.tasks@gmail.com>
2022-12-04 12:11:13 +0200

    fixing bugs

---
+++ app/src/main/java/com/udacity/asteroidradar/MainApplication.kt
@@ -0,0 +1,38 @@
+package com.udacity.asteroidradar
+
+import android.app.Application
+import androidx.lifecycle.ProcessLifecycleOwner.get
+import com.udacity.asteroidradar.database.AsteroidsDataBase
+import com.udacity.asteroidradar.database.DataSource
+import com.udacity.asteroidradar.database.MainRepository
+import com.udacity.asteroidradar.main.MainViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+import org.koin.android.viewmodel.dsl.viewModel
+
+class MainApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+
+        val myModule = module {
+
+            // Declare a ViewModel - be later inject into Fragment with dedicated injector using by viewModel()
+            viewModel {
+                MainViewModel(
+                    get() as DataSource,
+                    get()
+                )
+            }
+
+            single { MainRepository(get()) as DataSource}
+            single { AsteroidsDataBase.getInstance(this@MainApplication) }
+        }
+
+        startKoin {
+            androidContext(this@MainApplication)
+            modules(listOf(myModule))
+        }
+    }
+    }
+
--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -3,7 +3,7 @@ package com.udacity.asteroidradar.main
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import org.koin.android.viewmodel.dsl.viewModel
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
@@ -12,7 +12,7 @@ class MainFragment : Fragment() {
 //    private val viewModel: MainViewModel by lazy {
 //        ViewModelProvider(this).get(MainViewModel::class.java)
 //    }
-//    private val viewModel: MainViewModel by viewModel()
+    //private val viewModel: MainViewModel by viewModel()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
